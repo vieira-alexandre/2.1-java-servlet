8112f98595c6568e3c4da2572efe631fe206d372
alexandre-zup <alexandre.santos@zup.com.br>
2021-07-12 14:38:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/gerenciador/serlet/OiMundoServlet.java
@@ -0,0 +1,24 @@
+package br.com.zup.gerenciador.serlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi-mundo")
+public class OiMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String html = "<html>\n"
+				+ "<body>\n"
+				+ "<h3>Oi mundo, sou uma servlet!</h3>\n"
+				+ "</body>\n"
+				+ "</html>";
+		var out = resp.getWriter();
+		out.println(html);
+		System.out.println("OiMundoServlet foi chamada");
+	}
+}
